009b37788bd97415df3d247085b92df3b71f2bdb
mjmazur <mmazur5@uwo.ca>
2023-05-16 22:55:46 -0400

    Update RMSdashboard.py

--- RMSdashboard.py
+++ RMSdashboard.py
@@ -214,19 +214,22 @@ class Ui(QtWidgets.QDialog):
 		self.ui.setupUi(self.window)
 		self.window.show()
 
-	def handleVisibleChanged():
-		if not QtGui.QGuiApplication.inputMethod().isVisible():
-			return
-		for w in QtGui.QGuiApplication.allWindows():
-			if w.metaObject().className() == "QtVirtualKeyboard::InputView":
-				keyboard = w.findChild(QtCore.QObject, "keyboard")
-				if keyboard is not None:
-					r = w.geometry()
-					r.moveTop(keyboard.property("y"))
-					w.setMask(QtGui.QRegion(r))
-					return
+def handleVisibleChanged():
+	if not QtGui.QGuiApplication.inputMethod().isVisible():
+		return
+	for w in QtGui.QGuiApplication.allWindows():
+		if w.metaObject().className() == "QtVirtualKeyboard::InputView":
+			keyboard = w.findChild(QtCore.QObject, "keyboard")
+			if keyboard is not None:
+				r = w.geometry()
+				r.moveTop(keyboard.property("y"))
+				w.setMask(QtGui.QRegion(r))
+				return
 	
 if __name__ == "__main__":
+	os.environ['QT_IM_MODULE'] = 'qtvirtualkeyboard'
 	app = QtWidgets.QApplication(sys.argv)
+
+	QtGui.QGuiApplication.inputMethod().visibleChanged.connect(handleVisibleChanged)
 	window = Ui()
 	app.exec()
